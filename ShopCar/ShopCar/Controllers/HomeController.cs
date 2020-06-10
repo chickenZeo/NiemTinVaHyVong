17072adefb02f66147918ac3b09cadb36741cd2c
D.Quân <manhquan0105@gmail.com>
2020-06-10 17:54:39 +0700

    Up file Home index+ partial sanpham

--- ShopCar/ShopCar/Controllers/HomeController.cs
+++ ShopCar/ShopCar/Controllers/HomeController.cs
@@ -3,13 +3,23 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using ShopCar.Model;
 
 namespace ShopCar.Controllers
 {
     public class HomeController : Controller
     {
+        CarShopEntities db = new CarShopEntities();
         public ActionResult Index()
         {
+            ViewBag.lstChanNang = db.SanPhams.Where(x => x.MaLoaiSP == "L0003");
+
+            ViewBag.lstPhuKienKhac = db.SanPhams.Where(x => x.MaLoaiSP == "L0004");
+            ViewBag.lstDongCo = db.SanPhams.Where(x => x.MaLoaiSP == "L0005");
+            ViewBag.lstPhanh = db.SanPhams.Where(x => x.MaLoaiSP == "L0006");
+            ViewBag.lstDauNhot= db.SanPhams.Where(x => x.MaLoaiSP == "L0007");
+            ViewBag.lstSon = db.SanPhams.Where(x => x.MaLoaiSP == "L0008");
+
             return View();
         }
 
