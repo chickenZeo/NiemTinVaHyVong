3c2c32942054d334de00da606a8b040af58460bf
D.Quân <manhquan0105@gmail.com>
2020-06-10 16:30:40 +0700

    Up layout+home index

--- ShopCar/ShopCar/Controllers/HomeController.cs
+++ ShopCar/ShopCar/Controllers/HomeController.cs
@@ -3,28 +3,36 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using ShopCar.Model;
 
 namespace ShopCar.Controllers
 {
     public class HomeController : Controller
     {
+        CarShopEntities db = new CarShopEntities();
+        // GET: Home
         public ActionResult Index()
         {
-            return View();
-        }
-
-        public ActionResult About()
-        {
-            ViewBag.Message = "Your application description page.";
+            ViewBag.lstChanNang = db.SanPhams.Where(x => x.MaLoaiSP == "L0003");
+            ViewBag.lstPhuKienKhac = db.SanPhams.Where(x => x.MaLoaiSP == "L0004");
+            ViewBag.lstDauNhot = db.SanPhams.Where(x => x.MaLoaiSP == "L0007");
+            ViewBag.lstSon = db.SanPhams.Where(x => x.MaLoaiSP == "L0008");
 
             return View();
         }
-
-        public ActionResult Contact()
+        [HttpPost]
+        public ActionResult DangNhap(FormCollection f)
         {
-            ViewBag.Message = "Your contact page.";
+            string TenTK = f["txtTenDangNhap"].ToString();
+            string MatKhau = f["txtMatKhau"].ToString();
 
-            return View();
+            KhachHang tv1 = db.KhachHangs.SingleOrDefault(x => x.UserName == TenTK && x.Pass == MatKhau);
+            if (tv1 != null)
+            {
+                Session["TaiKhoan"] = tv1;
+                return Content("<script>window.location.reload();</script>");
+            }
+            return Content("<script>alert('Sai tài khoản hoặc mật khẩu')</script>");
         }
     }
 }
